334295ade1eb9ed9accdaf13776096efd47716a3
CabooseNomerson <bshs14bcarl@gmail.com>
2018-06-23 11:43:56 -0400

    Spawner functional - 6/23, 11:44am
    Spawner works, does not have an interval yet

---
+++ Game Camp 2018/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject Enemy;
+
+    [Header ("Screen Bounds")]
+
+	public float ScreenLeft;
+	public float ScreenRight;
+	[Space]
+	public float ScreenTop;
+	public float ScreenBottom;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector2(Random.Range(ScreenLeft, ScreenRight), Random.Range(ScreenTop, ScreenBottom));
+
+		Instantiate(Enemy, transform.position, Quaternion.identity);
+	}
+}
