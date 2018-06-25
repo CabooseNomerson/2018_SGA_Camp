446d948172b6ba36578a0719602c1037f0ddada2
CabooseNomerson <bshs14bcarl@gmail.com>
2018-06-25 13:23:22 -0400

    Rotation and MACs - 6/25, 13:23pm
    Got rotation targeting and MAC timers working

--- Game Camp 2018/Assets/Scripts/UNSCTargeting.cs
+++ Game Camp 2018/Assets/Scripts/UNSCTargeting.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class UNSCTargeting : MonoBehaviour {
 
-	private Transform Enemy;
+	private GameObject Enemy;
 
 	public float turnspeed;
 	public float MaxMagnitudeDelta;
 	private float TurnSpeed;
 
+	public float FireRate;
+	private float NextFire;
+
+
 	public GameObject MAC;
 	private Transform GoliathMAC;
 	private Transform DobMAC;
+	//private Vector3 TargetDifference;
 
 
 	// Use this for initialization
@@ -21,39 +26,51 @@ public class UNSCTargeting : MonoBehaviour {
 		GoliathMAC = GameObject.Find("GoliathMAC").transform;
 		DobMAC = GameObject.Find("DobMAC").transform;
 
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space) && Time.time > NextFire)
 		{
-			Instantiate(MAC, GoliathMAC.transform.position, Quaternion.identity);
-			Instantiate(MAC, DobMAC.transform.position, Quaternion.identity);
+            //reset the NextFire timer to a point that is FireRate seconds in the future
+			NextFire = Time.time + FireRate;
+
+			Instantiate(MAC, GoliathMAC.transform.position, transform.rotation);
+			Instantiate(MAC, DobMAC.transform.position, transform.rotation);
 		}
 
 		//Trying to get the UNSC ships to rotate toward enemies in a restricted angle
 
         
 
-		////constantly assign a target
-		//Enemy = GameObject.FindWithTag("Enemy").transform;
-
-		////find the direction of the target
-		//Vector3 TargetDifference = Enemy.position - transform.position;
-
-  //      //set the speed to turn
-		////TurnSpeed = Time.deltaTime * turnspeed;
-        
-  //      //set the Vector3 direction to rotate towards
-		////Vector3 NewDirection = Vector3.RotateTowards(transform.position, TargetDifference, TurnSpeed, MaxMagnitudeDelta);
+		//constantly assign a target
+		Enemy = GameObject.FindWithTag("Enemy");
 
 
-		//transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, Mathf.Atan2(TargetDifference.y, TargetDifference.x) * Mathf.Rad2Deg);
+		if (Enemy != null)
+		{
+			//find the direction of the target
+			Vector3 TargetDifference = Enemy.transform.position - transform.position;
 
-  //      //draw a debug gizmo
-		////Debug.DrawRay(transform.position, NewDirection, Color.red);
+			//      //set the speed to turn
+			////TurnSpeed = Time.deltaTime * turnspeed;
+
+			//set the Vector3 direction to rotate towards
+			Quaternion NewDirection = Quaternion.LookRotation(TargetDifference);
+			//Vector3 finalCheatDir = new Vector3(0, 0, NewDirection.eulerAngles.x);
+			//transform.eulerAngles= finalCheatDir;
+
+			transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, Mathf.Atan2(TargetDifference.y, TargetDifference.x) * Mathf.Rad2Deg);
+
+            //draw a debug gizmo
+			Debug.DrawRay(transform.position, TargetDifference, Color.red);
+		}
+
+
+	
 
 		////actually turn towards the target
 		////float rotationZ = 
